4bcd4f8c38b73e017d2023e5729174322ee3e1a2
wangxiang <wangxiang@yixia.com>
2018-08-14 11:56:31 +0800

    Initial Commit

---
+++ AlgorithmPractice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AlgorithmPractice
+//
+//  Created by shine on 2018/8/14.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
