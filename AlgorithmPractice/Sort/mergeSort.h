e338e001d87d217a9c5d4ee0802342890edea81f
wangxiang <wangxiang@yixia.com>
2018-12-04 10:50:25 +0800

    mergeSort

---
+++ AlgorithmPractice/Sort/mergeSort.h
@@ -0,0 +1,17 @@
+//
+//  mergeSort.h
+//  AlgorithmPractice
+//
+//  Created by shine on 2018/11/17.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#ifndef mergeSort_h
+#define mergeSort_h
+
+#include <stdio.h>
+
+//归并排序
+void mergeSort(int arr[], int len);
+
+#endif /* mergeSort_h */
